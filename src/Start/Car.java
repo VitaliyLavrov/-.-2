aa56bdcb31dd3588bedce446bdbc1b84ffa1e125
VitaliyLavrov <LavroWitaliy@gmail.com>
2021-03-13 07:25:26 +0200

    Многопоточность. Часть 2

---
+++ src/Start/Car.java
@@ -0,0 +1,57 @@
+package Start;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicIntegerArray;
+
+public class Car implements Runnable {
+    private static int CARS_COUNT;
+    private Race race;
+    private int speed;
+    private String name;
+    CyclicBarrier barrier;
+    private static final AtomicInteger position = new AtomicInteger();
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public Car(Race race, int speed, CyclicBarrier barrier) {
+        this.race = race;
+        this.speed = speed;
+        CARS_COUNT++;
+        this.name = "Участник #" + CARS_COUNT;
+        this.barrier = barrier;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println(this.name + " готовится");
+            Thread.sleep(500 + (int) (Math.random() * 800));
+            System.out.println(this.name + " готов");
+            barrier.await();
+
+            barrier.await();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < race.getStages().size(); i++) {
+            race.getStages().get(i).go(this);
+        }
+        System.out.println(this.name+ " занял " + position.incrementAndGet() + " место");
+            try {
+                barrier.await();
+
+            }catch (InterruptedException | BrokenBarrierException e){
+                e.printStackTrace();
+            }
+
+    }
+
+}
